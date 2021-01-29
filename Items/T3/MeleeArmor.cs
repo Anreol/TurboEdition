3448f8b7a5c1f7987e937bb0c7bd80f798806c97
Anreol <null>
2021-01-29 13:41:52 +0100

    Fixed formatting

--- Items/T3/MeleeArmor.cs
+++ Items/T3/MeleeArmor.cs
@@ -10,12 +10,12 @@ namespace TurboEdition.Items
 
         public override string ItemLangTokenName => "MELEE_ARMOR";
 
-        public override string ItemPickupDesc => "Son!";
+        public override string ItemPickupDesc => "THEY RESPONSE TO PHYSICAL TRAUMA.";
 
-        public override string ItemFullDescription => "THEY RESPONSE TO PHYSICAL TRAUMA";
+        public override string ItemFullDescription => $"When damaged by an enemy within <style=cIsUtility>{rangeRadius} meters</style>, gain <style=cIsUtility>Nanomachines</style> {buffPerStack} times. <style=cStack>(+{buffPerStack} per stack).</style>";
 
-        public override string ItemLore => "You are right about one thing.\nI do need capital. And votes.\nWanna know why?\n'I have a dream'\n\nWhat?\n\nThat one day every person in this nation will control their OWN destiny.\nA land of the TRULY free, damnit.\nA nation of ACTION, not words.\nRuled by STRENGHT, not committee.\nWhere the law changes to suit the individual, not the other way around\nWhere power and justice are back where they belong: in the hands of the people!\nWhere every man is free to think -- to act -- for himself!\n" +
-                                            "Fuck all these limp-dick lawyers and chicken-shit bureaucrats.\nFuck their 24/7 internet spew of trivia and celebrity bullshit.\nFuck 'American Pride.' Fuck the media!\nFuck all of it!\nAmerica is diseased. Rotten to the core.\nThere's no saving it -- we need to pull it out by the roots.\nWipe the slate clean. BURN IT DOWN!\nAnd from the ashes a new America will be born.\nEvolved, but untamed!\nThe weak will be purged, and the strongest will thrive -- free to live as they see fit,\nthey'll make America great again!\n\nWhat the hell are you talking about...";
+        public override string ItemLore => "You are right about <style=cUserSetting>one thing.</style>\nI do need <style=cIsUtility>capital</style>. And <style=cIsUtility>votes.</style>\nWanna know why?\n<style=cKeywordName>'I have a dream'</style><style=cMono>What?</style>\nThat one day <style=cIsUtility>every person in this nation</style> will control their <style=cIsHealing>OWN</style> destiny.\nA land of the <style=cIsHealing>TRULY</style> free, damnit.\nA nation of <style=cIsHealing>ACTION</style>, <style=cDeath>not words</style>.\nRuled by <style=cIsHealing>STRENGHT</style>, <style=cDeath>not committee</style>.\nWhere the law changes <style=cIsUtility>to suit the individual</style>, not the other way around\nWhere <style=cIsDamage>power</style> and <style=cIsHealth>justice</style> are back where they belong: <style=cIsUtility>in the hands of the people!</style>\nWhere every man is free to think -- <style=cIsUtility>to act</style> -- for <style=cIsHealing>himself</style>!\n" +
+                                            "Fuck all these <style=cArtifact>limp-dick lawyers</style> and <style=cDeath>chicken-shit bureaucrats</style>.\nFuck <style=cIsHealth>their 24/7 internet spew of trivia</style> and <style=cDeath>celebrity bullshit.</style>\nFuck <style=cKeywordName>'American Pride.'</style><style=cUserSetting>Fuck the media!</style>\nFuck all of it!\nAmerica is <style=cDeath>diseased</style>. <style=cArtifact>Rotten to the core.</style>\nThere's no saving it -- <style=cIsUtility>we need to pull it out by the roots.</style>\nWipe the slate <style=cEvent>clean</style>. <style=cDeath>BURN IT DOWN!</style>\nAnd from the ashes a <style=cUserSetting>new America</style> will be born.\n<style=cIsUtility>Evolved</style>, but <style=cIsDamage>untamed</style>!\nThe <style=cStack>weak</style> will be <style=cDeath>purged</style>, and the <style=cStack>strongest</style> will <style=cIsHealing>thrive</style> -- <style=cIsUtility>free to live as they see fit</style>,\n<style=cShrine>they'll make America great again!</style>\n\n<style=cMono>What the hell are you talking about...</style>";
 
         public override ItemTier Tier => ItemTier.Tier3;
 
@@ -24,9 +24,14 @@ namespace TurboEdition.Items
         public override string ItemIconPath => "@TurboEdition:Assets/Textures/Icons/Items/Tier3.png";
 
 
+        //Item properties
+        public float rangeRadius;
+        public float buffPerStack;
+
         public override void CreateConfig(ConfigFile config)
         {
-
+            rangeRadius = config.Bind<float>("Item: " + ItemName, "Radius in meters", 6f, "If an enemy within this range hits the user, it will add meleearmor buff").Value;
+            buffPerStack = config.Bind<int>("Item: " + ItemName, "Buff per stack", 1, "Number of meleearmor to apply when hit.").Value;
         }
 
         public override ItemDisplayRuleDict CreateItemDisplayRules()
