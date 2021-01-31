2ca2b28014e2e8315c47f5374dea045445401303
Anreol <null>
2021-01-31 18:09:32 +0100

    I 👏 dont 👏 know 👏 what 👏 i 👏 can 👏 do

--- TE/Equipment/Normal/StageSkip.cs
+++ TE/Equipment/Normal/StageSkip.cs
@@ -33,7 +33,7 @@ namespace TurboEdition.Equipment
 
         public float equipmentRecharge;
         public SceneExitController sceneExitController { get; private set; }
-        public bool extraSceneAdded;
+
 
         protected override void CreateConfig(ConfigFile config)
         {
@@ -53,36 +53,29 @@ namespace TurboEdition.Equipment
         //We could probably do this via the game's own SceneExitController but some stages such as the Ambry doesn't have one so we'll create one.
         protected override bool ActivateEquipment(EquipmentSlot slot)
         {
-            if (!extraSceneAdded)
+            int sceneExitCount = InstanceTracker.GetInstancesList<SceneExitController>().Count;
+            #if DEBUG
+            TurboEdition._logger.LogWarning("sceneExitCount: " + sceneExitCount);
+            #endif
+            if (TeleporterInteraction.instance || !SceneExitController.isRunning)
             {
-                int sceneExitCount = InstanceTracker.GetInstancesList<SceneExitController>().Count;
-#if DEBUG
-                TurboEdition._logger.LogWarning("sceneExitCount: " + sceneExitCount);
-#endif
-                if (TeleporterInteraction.instance || !SceneExitController.isRunning)
+                //Makes sure that theres only the game's own controller, if theres none at least it wont create more than two
+                if (sceneExitCount <= 1)
                 {
-                    //Makes sure that theres only the game's own controller, if theres none at least it wont create more than two
-                    if (sceneExitCount <= 1)
+                    if (TeleporterInteraction.instance.chargeFraction >= 0.99 || Reflection.GetFieldValue<bool>(TeleporterInteraction.instance, "monstersCleared"))
                     {
-                        if (TeleporterInteraction.instance.chargeFraction >= 0.99 || Reflection.GetFieldValue<bool>(TeleporterInteraction.instance, "monstersCleared"))
-                        {
-                            if (!extraSceneAdded)
-                            {
-                                SceneExitController sceneExitController = new SceneExitController();
-                                SkipStage(sceneExitController);
-                                extraSceneAdded = true;
-                                return true;
-                            }
-                        }
+                        SceneExitController sceneExitController = new SceneExitController();
+                        SkipStage(sceneExitController);
+                        return true;
                     }
                 }
-#if DEBUG
-                Chat.AddMessage("Turbo Edition: " + EquipmentName + " couldn't skip stage, see log for details.");
-                TurboEdition._logger.LogWarning("chargeFraction: " + TeleporterInteraction.instance.chargeFraction);
-                TurboEdition._logger.LogWarning("monstersCleared: " + Reflection.GetFieldValue<bool>(TeleporterInteraction.instance, "monstersCleared"));
-                TurboEdition._logger.LogWarning("isRunning: " + SceneExitController.isRunning);
-#endif
             }
+            #if DEBUG
+            Chat.AddMessage("Turbo Edition: " + EquipmentName + " couldn't skip stage, see log for details.");
+            TurboEdition._logger.LogWarning("chargeFraction: " + TeleporterInteraction.instance.chargeFraction);
+            TurboEdition._logger.LogWarning("monstersCleared: " + Reflection.GetFieldValue<bool>(TeleporterInteraction.instance, "monstersCleared"));
+            TurboEdition._logger.LogWarning("isRunning: " + SceneExitController.isRunning);
+            #endif
             return false;
         }
 
