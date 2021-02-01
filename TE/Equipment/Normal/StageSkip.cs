579b2c6ed86a9284d663c594d3861bd6d547a3ac
Anreol <null>
2021-02-01 13:54:44 +0100

    Made StageSkip check if current stage is an stage, final stage and moon While is not ideal at least it wont throw errors

--- TE/Equipment/Normal/StageSkip.cs
+++ TE/Equipment/Normal/StageSkip.cs
@@ -32,8 +32,15 @@ namespace TurboEdition.Equipment
         public override float Cooldown => equipmentRecharge;
 
         public float equipmentRecharge;
-        public SceneExitController sceneExitController { get; private set; }
 
+        public SceneExitController explicitSceneExitController { get;  }
+        private bool canSkipCurrentStage
+        {
+            get
+            {
+                return SceneCatalog.mostRecentSceneDef.sceneType == SceneType.Stage;
+            }
+        }
 
         protected override void CreateConfig(ConfigFile config)
         {
@@ -50,21 +57,49 @@ namespace TurboEdition.Equipment
 
         }
 
+
+        //IMPORTANT
+        //AAAAAAAAA
+        //CURRENTLY DOESN'T WORK IN HIDDEN REALMS
+        //WHILE IS UP TO CHOICE IF IT DOES OR NOT WHAT I MEAN BY THIS IS THAT IT THROWS A NULL EXCEPTION!!!!!!!!
+
         //We could probably do this via the game's own SceneExitController but some stages such as the Ambry doesn't have one so we'll create one.
         protected override bool ActivateEquipment(EquipmentSlot slot)
         {
+            if (Stage.instance.sceneDef.isFinalStage || UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "moon")
+            {
+#if DEBUG
+                Chat.AddMessage("Turbo Edition: " + EquipmentName + " this is a final stage, won't skip!");
+#endif
+                return false;
+            }
+            if (!(SceneCatalog.mostRecentSceneDef.sceneType == SceneType.Stage))
+            {
+#if DEBUG       
+                Chat.AddMessage("Turbo Edition: " + EquipmentName + " current scene is not a stage, won't skip!");
+#endif
+                return false;
+            }
             int sceneExitCount = InstanceTracker.GetInstancesList<SceneExitController>().Count;
             #if DEBUG
             TurboEdition._logger.LogWarning("sceneExitCount: " + sceneExitCount);
             #endif
             if (TeleporterInteraction.instance || !SceneExitController.isRunning)
             {
-                //Makes sure that theres only the game's own controller, if theres none at least it wont create more than two
-                if (sceneExitCount <= 1)
+                //Makes sure that theres only the game's own controller, if theres none at least it wont create more than three
+                //Increased to two since Bazaar has two
+                if (sceneExitCount <= 2)
                 {
                     if (TeleporterInteraction.instance.chargeFraction >= 0.99 || Reflection.GetFieldValue<bool>(TeleporterInteraction.instance, "monstersCleared"))
                     {
-                        SceneExitController sceneExitController = new SceneExitController();
+                        SceneExitController sceneExitController = explicitSceneExitController;
+                        if(!sceneExitController)
+                        {
+                            sceneExitController = InstanceTracker.FirstOrNull<SceneExitController>();
+#if DEBUG
+                            TurboEdition._logger.LogWarning("instanceTracker.FirstOrNull: " + sceneExitController);
+#endif
+                        }
                         SkipStage(sceneExitController);
                         return true;
                     }
@@ -79,24 +114,30 @@ namespace TurboEdition.Equipment
             return false;
         }
 
-        void SkipStage(SceneExitController sceneExitController)
+        void SkipStage(SceneExitController self)
         {
-            //sceneExitController = .GetComponent<SceneExitController>();
-            InstanceTracker.Add(sceneExitController);
-#if DEBUG
+            /*I actually dont know what this does
+            //self = explicitSceneExitController.GetComponent<SceneExitController>();
+            #if DEBUG
+            Chat.AddMessage("Turbo Edition: " + EquipmentName + " getComponent " + self);
+            #endif*/
+            //CANNOT DO THIS!!!! Throws an exception on ExitStage.State newState!
+            //self = new SceneExitController();
+
+            InstanceTracker.Add(self);
+            #if DEBUG
             Chat.AddMessage("Turbo Edition: " + EquipmentName + " forced stage skip.");
-#endif
+            #endif
             //The game does this so lets do too
             if (NetworkServer.active)
             {
                 //SceneExitController.ExitState == SceneExitController.ExitState.Idle;
-                //sceneExitController.SetState(SceneExitController.ExitState(idle));
-                sceneExitController.Begin();
+                self.SetState(SceneExitController.ExitState.Idle);
+                self.Begin();
             }
-            // TODO instance of this sceneExitController has to be removed after the item gets used
-            //Not sure if this will work since we havent gotten this far yet
-            //InstanceTracker.Remove(sceneExitController);
+            //If we are grabbing an already existing instance from the game thanks to InstanceTracker.FirstOrNull we are deleting that one
+            //should be fine since we are leaving the scene though.
+            InstanceTracker.Remove(self);
         }
-
     }
 }
