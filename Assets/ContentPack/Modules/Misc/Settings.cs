db4740b4fa30caf2c25e362644a64da7cc39593d
Anreol <12176932+Anreol@users.noreply.github.com>
2021-12-07 14:51:20 +0100

    commiting just in case

--- Assets/ContentPack/Modules/Misc/Settings.cs
+++ Assets/ContentPack/Modules/Misc/Settings.cs
@@ -16,14 +16,14 @@ namespace TurboEdition.Misc
         private static void Init()
         {
             SceneCatalog.onMostRecentSceneDefChanged += SceneCatalog_onMostRecentSceneDefChanged;
-            PauseManager.onPauseStartGlobal += new Action (delegate() {
+            PauseManager.onPauseStartGlobal += new Action(delegate ()
+            {
                 PauseAction();
             });
         }
 
         public static GameObject panel = Assets.mainAssetBundle.LoadAsset<GameObject>("SettingsSubPanel, Turbo");
         public static GameObject headerButton = Assets.mainAssetBundle.LoadAsset<GameObject>("GenericHeaderButton (Turbo)");
-        public static RoR2.UI.MainMenu.SubmenuMainMenuScreen currentSmms = null;
 
         private static void SceneCatalog_onMostRecentSceneDefChanged(SceneDef obj)
         {
@@ -33,13 +33,10 @@ namespace TurboEdition.Misc
                 RoR2.UI.MainMenu.MainMenuController mmc = mm.GetComponent<RoR2.UI.MainMenu.MainMenuController>();
                 if (mmc.settingsMenuScreen)
                 {
-                    mmc.settingsMenuScreen.onEnter.AddListener(new UnityAction(MainMenuSettings));
-                    currentSmms = mmc.settingsMenuScreen.GetComponent<RoR2.UI.MainMenu.SubmenuMainMenuScreen>();
-                    if (currentSmms != null)
+                    mmc.settingsMenuScreen.onEnter.AddListener(new UnityAction(delegate ()
                     {
-                        TELog.logger.LogWarning("Adding listener to currentSmms");
-                        //currentSmms.onEnter.AddListener(new UnityAction(MainMenuSettings));
-                    }
+                        TurboEdition.instance.StartCoroutine(Trolltine(mmc.settingsMenuScreen.GetComponent<RoR2.UI.MainMenu.SubmenuMainMenuScreen>()));
+                    }));
                 }
             }
         }
@@ -48,27 +45,13 @@ namespace TurboEdition.Misc
         {
             if (!PauseManager.isPaused || SceneCatalog.mostRecentSceneDef == SceneCatalog.GetSceneDefFromSceneName("title"))
                 return;
-            PauseManager.pauseScreenInstance.transform.Find("Blur + Background Panel/ValidScreenspacePanel/MainPanel/OptionsPanel (JUICED)/GenericMenuButton (Settings)").GetComponent<RoR2.UI.HGButton>().onClick.AddListener(new UnityAction(PauseMenuSettings));
+            PauseManager.pauseScreenInstance.transform.Find("Blur + Background Panel/ValidScreenspacePanel/MainPanel/OptionsPanel (JUICED)/GenericMenuButton (Settings)").GetComponent<RoR2.UI.HGButton>().onClick.AddListener(delegate ()
+            {
+                TurboEdition.instance.StartCoroutine(Trolltine(PauseManager.pauseScreenInstance.GetComponent<PauseScreenController>()));
+            });
             return;
         }
-        private static void MainMenuSettings()
-        {
-            TurboEdition.instance.StartCoroutine(Trolltine(currentSmms));
-            /*if (currentSmms.submenuPanelInstance)
-            {
-                TELog.logger.LogWarning("Assigning to settings menu.");
-                AssignMenu(currentSmms.submenuPanelInstance);
-            }*/
-        }
-        private static void PauseMenuSettings()
-        {
-            TELog.logger.LogWarning(PauseManager.pauseScreenInstance.GetComponent<RoR2.UI.PauseScreenController>().submenuObject);
-            //TODO TurboEdition.instance.StartCoroutine(Trolltine(PauseManager.pauseScreenInstance.GetComponent<PauseScreenController>().submenuObject));
-            /*if (PauseManager.pauseScreenInstance.GetComponent<RoR2.UI.PauseScreenController>().submenuObject)
-            {
-                AssignMenu(PauseManager.pauseScreenInstance.GetComponent<RoR2.UI.PauseScreenController>().submenuObject);
-            }*/
-        }
+
         private static void AssignMenu(GameObject submenuPanelInstance)
         {
             Transform header = submenuPanelInstance.transform.Find("SafeArea/HeaderContainer/Header (JUICED)");
@@ -79,10 +62,13 @@ namespace TurboEdition.Misc
 
             RoR2.UI.HGButton leButton = headerInstance.GetComponent<RoR2.UI.HGButton>();
             RoR2.UI.HGHeaderNavigationController leHeader = submenuPanelInstance.GetComponent<RoR2.UI.HGHeaderNavigationController>();
-            //leButton.onClick.AddListener(new UnityAction (ChooseHeaderByButton())); //TODO ADD LISTENER TO HEADER ChooseHeaderByButton()
+            leButton.onClick.AddListener((delegate ()
+            {
+                leHeader.ChooseHeaderByButton(leButton);
+            }));
 
             panelInstance.GetComponent<RoR2.UI.SettingsPanelController>().revertButton = submenuPanelInstance.transform.Find("FooterContainer/FooterPanel, M&KB/RevertAndBack (JUICED)/NakedButton (Revert)").GetComponent<RoR2.UI.MPButton>();
-            
+
             //panelInstance.GetComponent<RoR2.UI.HGButtonHistory>().requiredTopLayer = submenuPanelInstance.GetComponentInChildren<RoR2.UI.UILayerKey>();
             //panelInstance.GetComponent<RoR2.UI.HGScrollRectHelper>().requiredTopLayer = submenuPanelInstance.GetComponentInChildren<RoR2.UI.UILayerKey>();
 
@@ -99,14 +85,18 @@ namespace TurboEdition.Misc
             int lastOption = header.Find("GenericHeaderButton (Graphics)").GetSiblingIndex() + 1;
             headerInstance.transform.SetSiblingIndex(lastOption);
             header.Find("GenericGlyph (Right)").SetSiblingIndex(headerInstance.transform.GetSiblingIndex() + 1);
-
         }
 
         private static IEnumerator Trolltine(RoR2.UI.MainMenu.SubmenuMainMenuScreen trolled) //Me getting trolled by one single line of code
         {
             yield return new WaitForEndOfFrame();
-            TELog.logger.LogWarning("Waited til the end of frame");
             AssignMenu(trolled.submenuPanelInstance);
+        }
+
+        private static IEnumerator Trolltine(RoR2.UI.PauseScreenController trolled) //Me getting trolled by one single line of code
+        {
+            yield return new WaitForEndOfFrame();
+            AssignMenu(trolled.submenuObject);
         }
     }
 }
