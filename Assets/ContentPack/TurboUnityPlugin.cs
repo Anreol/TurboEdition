99df129499f16c5c66c4d6ba48275977cbd2c501
Anreol <12176932+Anreol@users.noreply.github.com>
2022-06-04 19:59:51 +0200

    some more item polish

--- Assets/ContentPack/TurboUnityPlugin.cs
+++ Assets/ContentPack/TurboUnityPlugin.cs
@@ -1,9 +1,6 @@
 ﻿using BepInEx;
 using RoR2;
 using RoR2.ContentManagement;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using UnityEngine;
 
 //Dumbfuck's first (not really) ror2 mod
@@ -37,11 +34,58 @@ namespace TurboEdition
 #endif
             pluginInfo = Info;
             instance = this;
-
             ContentManager.collectContentPackProviders += (addContentPackProvider) => addContentPackProvider(new TEContent());
 #if DEBUG
             //Components.MaterialControllerComponents.AttachControllerFinderToObjects(Assets.mainAssetBundle);
 #endif
+
+            //RoR2Application.onFixedUpdate += onFixedUpdate;
+            //UnityEngine.SceneManagement.SceneManager.activeSceneChanged += activeSceneChanged;
+        }
+
+        private void activeSceneChanged(UnityEngine.SceneManagement.Scene current, UnityEngine.SceneManagement.Scene next)
+        {
+            if (next.isLoaded && next.name == "renderitem")
+            {
+                GameObject parent = GameObject.Find("ITEM GOES HERE (can offset from here)");
+                GameObject light = null; //Thing is disabled, wont be found by GameObject.Find
+                foreach (var item in next.GetRootGameObjects())
+                {
+                    if (item.name == "move me for pleasant local lighting details")
+                    {
+                        light = item;
+                    }
+                }
+
+                if (light != null)
+                {
+                    GameObject primitive = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    primitive.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+                    primitive.transform.SetParent(light.transform);
+                }
+                foreach (System.Reflection.FieldInfo itemdef in typeof(TEContent.Items).GetFields())
+                {
+                    ItemDef itemDef = (ItemDef)itemdef.GetValue(null);
+                    if (!itemDef.pickupModelPrefab || itemDef.pickupModelPrefab.name.Contains("Placeholder"))
+                        continue;
+                    if (parent)
+                    {
+                        UnityEngine.Object.Instantiate(itemDef.pickupModelPrefab, parent.transform).SetActive(false);
+                    }
+                    else
+                    {
+                        UnityEngine.Object.Instantiate(itemDef.pickupModelPrefab, new Vector3(-0.06f, 0.04f, 2.07f), Quaternion.Euler(0f, 210.6264f, 0f)).SetActive(false);
+                    }
+                }
+            }
+        }
+
+        private void onFixedUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                UnityEngine.AddressableAssets.Addressables.LoadSceneAsync("RoR2/Dev/renderitem/renderitem.unity", UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
         }
     }
 }
