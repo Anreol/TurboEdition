e59c53173f26d5e823539aac68accec626564cac
Anreol <12176932+Anreol@users.noreply.github.com>
2022-06-14 17:36:33 +0200

    stupid shit

---
+++ Assets/ContentPack/Modules/Components/MarkAsUnableToBeLocked.cs
@@ -0,0 +1,43 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using System;
+using UnityEngine;
+
+namespace TurboEdition.Components
+{
+    public class MarkAsUnableToBeLockedHook
+    {
+        [SystemInitializer]
+        public static void Initialize()
+        {
+            IL.RoR2.OutsideInteractableLocker.ChestLockCoroutine += ChestLockCoroutine;
+        }
+
+        private static void ChestLockCoroutine(MonoMod.Cil.ILContext il)
+        {
+            var c = new ILCursor(il);
+
+            if (c.TryGotoNext(MoveType.After, x => x.MatchLdloc(10), x => x.MatchCallOrCallvirt<UnityEngine.Object>("op_Implicit")))
+            {
+                bool hasMarker = false;
+                c.Emit(OpCodes.Ldloc, 10);
+                c.EmitDelegate<Action<OutsideInteractableLocker.Candidate>>(candidate =>
+                {
+                    if (candidate.purchaseInteraction.gameObject.GetComponent<MarkAsUnableToBeLocked>() != null)
+                    {
+                        hasMarker = true;
+                    }
+                });
+                c.EmitDelegate<Func<bool, bool>>((origBool) =>
+                {
+                    return origBool && hasMarker;
+                });
+            }
+        }
+    }
+    public class MarkAsUnableToBeLocked : MonoBehaviour
+    {
+
+    }
+}
--- Assets/ContentPack/Modules/Misc/CostAndStatExtras.cs
+++ Assets/ContentPack/Modules/Misc/CostAndStatExtras.cs
@@ -37,55 +37,6 @@ namespace TurboEdition.Misc
             c.Emit(OpCodes.Ldarg_0);
             c.EmitDelegate<Action<PageBuilder>>(build => userProfile = (UserProfile)build.entry.extraData);
 
-            /*if (c.TryGotoNext(MoveType.After, x => x.MatchCastclass<UserProfile>(), x => x.MatchStloc(out _)))
-            {
-                c.Emit(OpCodes.Ldloc_1);
-                c.EmitDelegate<Action<UserProfile>>(userProf => { UnityEngine.Debug.Log(userProf); userProfile = userProf;});
-
-            }*/
-            /*
-            //Go to the first addition to the array
-            c.GotoNext(x => x.OpCode == OpCodes.Ldsfld && x.MatchLdsfld(typeof(StatDef), nameof(RoR2.Stats.StatDef.totalGamesPlayed)));
-
-            //Go back and find instructions with a local int and right after it a new array
-            int baseArrayLength = 0;
-            if (c.TryGotoPrev(x => x.MatchLdcI4(out baseArrayLength), x => x.MatchNewarr<ValueTuple<string, string, UnityEngine.Texture>>()))
-            {
-                //Get the int that sets the length of the array, add X to it. Next.Operand because the cursor, when moving, by default moves BEFORE the match it has found.
-                c.Next.Operand = (sbyte)(baseArrayLength + 1);
-
-                //find instructions with this same order, to position ourselves and to append our entry before it.
-                if (c.TryGotoNext(x => x.MatchLdcI4(out _), x => x.MatchLdloc(out _), x => x.MatchLdsfld(typeof(StatDef), nameof(RoR2.Stats.StatDef.totalCrocoInfectionsInflicted))))
-                {
-                    //Add our new entry to array
-                    /*c.Emit(OpCodes.Ldloc_0);
-                    c.EmitDelegate<Action<ValueTuple<string, string, UnityEngine.Texture>[]>>(array =>
-                    {
-                        ValueTuple<string, string, UnityEngine.Texture> result;
-                        result.Item1 = Language.GetString(totalTeleporterOverchargerUsed.displayToken);
-                        result.Item2 =
-                        HG.ArrayUtils.ArrayAppend<ValueTuple<string, string, UnityEngine.Texture>>(ref array, result);
-                    });*/
-
-            //After our new entry has been added, find every next entries index. This might cause issues if appending is done at the very last entry.
-            /*do
-            {
-                //Then add one to their index so they are added to the array X elements later, compensanting for our new element.
-                int index = -1;
-                c.GotoNext(x => x.MatchLdcI4(out index));
-                if (index <= 255)
-                {
-                    c.Next.Operand = (sbyte)(index + 1);
-                }
-                else //if its larger than a byte, change opcode type to support longer numbers
-                {
-                    c.Next.OpCode = OpCodes.Ldc_I4;
-                    c.Next.Operand = (index + 1);
-                }
-            }
-            while (c.TryGotoNext(MoveType.After, x => x.MatchNewobj<ValueTuple<string, string, UnityEngine.Texture>>(), x => x.MatchStelemAny<ValueTuple<string, string, UnityEngine.Texture>>()) && c.Next.OpCode == OpCodes.Dup);
-        }
-    }*/
             if (c.TryGotoNext(MoveType.After, x => x.MatchStelemAny<ValueTuple<string, string, UnityEngine.Texture>>(), x => x.MatchLdloca(out _), x => x.MatchCallOrCallvirt(typeof(RoR2.UI.LogBook.PageBuilder).GetMethod("<StatsPanel>g__SetStats|26_13", (System.Reflection.BindingFlags)(-1)))))
             {
                 if (c.TryGotoPrev(x => x.MatchLdloca(out _)))
@@ -93,7 +44,11 @@ namespace TurboEdition.Misc
                     c.EmitDelegate<Func<ValueTuple<string, string, UnityEngine.Texture>[], ValueTuple<string, string, UnityEngine.Texture>[]>>(array =>
                     {
                         List<ValueTuple<string, string, UnityEngine.Texture>> newArray = new List<ValueTuple<string, string, UnityEngine.Texture>>(array);
+                        
+                        //totalTeleporterOverchargerUsed stat
                         newArray.Insert(17, (Language.GetString(totalTeleporterOverchargerUsed.displayToken), userProfile.statSheet.GetStatDisplayValue(totalTeleporterOverchargerUsed), null));
+                        
+                        //Return
                         return newArray.ToArray();
                     });
                 }
