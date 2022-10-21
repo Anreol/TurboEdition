de757d9f8ac49a23a81e4f5a382fcb33aa9d4a8a
Anreol <12176932+Anreol@users.noreply.github.com>
2022-10-21 10:56:20 +0200

    awawawawawa

--- Assets/ContentPack/Modules/Misc/UIExtras.cs
+++ Assets/ContentPack/Modules/Misc/UIExtras.cs
@@ -20,14 +20,14 @@ namespace TurboEdition.Misc
                 return;
             CameraRigController.onCameraEnableGlobal += onCameraEnabledGlobal;
             SceneCatalog.onMostRecentSceneDefChanged += onMostRecentSceneDefChanged;
-            PauseManager.onPauseStartGlobal += new Action(delegate ()
-            {
-                PauseAction();
-            });
+            //PauseManager.onPauseStartGlobal += new Action(delegate ()
+            //{
+            //    PauseAction();
+            //});
         }
 
-        public static GameObject panel = Assets.mainAssetBundle.LoadAsset<GameObject>("SettingsSubPanel, Turbo");
-        public static GameObject headerButton = Assets.mainAssetBundle.LoadAsset<GameObject>("GenericHeaderButton (Turbo)");
+        //public static GameObject panel = Assets.mainAssetBundle.LoadAsset<GameObject>("SettingsSubPanel, Turbo");
+        //public static GameObject headerButton = Assets.mainAssetBundle.LoadAsset<GameObject>("GenericHeaderButton (Turbo)");
 
         //public static GameObject statBarContainer = Assets.mainAssetBundle.LoadAsset<GameObject>("StatBarsContainer");
         public static GameObject scoreboardLeftSidePanel = Assets.mainAssetBundle.LoadAsset<GameObject>("ScoreboardLeftSidePanel");
@@ -49,13 +49,13 @@ namespace TurboEdition.Misc
                 mainMenuObject = GameObject.Find("MainMenu");
                 RoR2.UI.MainMenu.MainMenuController mmc = mainMenuObject.GetComponent<RoR2.UI.MainMenu.MainMenuController>();
                 //RoR2.UI.MainMenu.MainMenuController mmc = RoR2.UI.MainMenu.MainMenuController.instance; THIS FAILS. GOD DAMNIT HOPOO
-                if (mmc.settingsMenuScreen)
-                {
-                    mmc.settingsMenuScreen.onEnter.AddListener(new UnityAction(delegate ()
-                    {
-                        TurboUnityPlugin.instance.StartCoroutine(AddSettingsMenuCoroutine(mmc.settingsMenuScreen.GetComponent<RoR2.UI.MainMenu.SubmenuMainMenuScreen>()));
-                    }));
-                }
+                //if (mmc.settingsMenuScreen)
+                //{
+                //    mmc.settingsMenuScreen.onEnter.AddListener(new UnityAction(delegate ()
+                //    {
+                //        TurboUnityPlugin.instance.StartCoroutine(AddSettingsMenuCoroutine(mmc.settingsMenuScreen.GetComponent<RoR2.UI.MainMenu.SubmenuMainMenuScreen>()));
+                //    }));
+                //}
                 if (mmc.multiplayerMenuScreen)
                 {
                     GameObject itemHolder = mmc.multiplayerMenuScreen.transform.parent.GetChild(0).GetChild(1).GetChild(1).GetChild(26).GetChild(4).gameObject;
@@ -75,16 +75,16 @@ namespace TurboEdition.Misc
             }
         }
 
-        private static void PauseAction()
-        {
-            if (!PauseManager.isPaused || SceneCatalog.mostRecentSceneDef.isOfflineScene)
-                return;
-            PauseManager.pauseScreenInstance.transform.Find("Blur + Background Panel/ValidScreenspacePanel/MainPanel/OptionsPanel (JUICED)/GenericMenuButton (Settings)").GetComponent<RoR2.UI.HGButton>().onClick.AddListener(delegate ()
-            {
-                TurboUnityPlugin.instance.StartCoroutine(AddSettingsMenuCoroutine(PauseManager.pauseScreenInstance.GetComponent<PauseScreenController>()));
-            });
-            return;
-        }
+        //private static void PauseAction()
+        //{
+        //    if (!PauseManager.isPaused || SceneCatalog.mostRecentSceneDef.isOfflineScene)
+        //        return;
+        //    PauseManager.pauseScreenInstance.transform.Find("Blur + Background Panel/ValidScreenspacePanel/MainPanel/OptionsPanel (JUICED)/GenericMenuButton (Settings)").GetComponent<RoR2.UI.HGButton>().onClick.AddListener(delegate ()
+        //    {
+        //        TurboUnityPlugin.instance.StartCoroutine(AddSettingsMenuCoroutine(PauseManager.pauseScreenInstance.GetComponent<PauseScreenController>()));
+        //    });
+        //    return;
+        //}
         private static void PatchHUD(HUD newHud)
         {
             ChildLocator cl = newHud.GetComponent<ChildLocator>();
@@ -125,48 +125,48 @@ namespace TurboEdition.Misc
             UnityEngine.Object.Instantiate(panel, parent).SetActive(true);
         }
 
-        private static void AssignMenu(GameObject submenuPanelInstance)
-        {
-            Transform header = submenuPanelInstance.transform.Find("SafeArea/HeaderContainer/Header (JUICED)");
-            Transform subPanelArea = submenuPanelInstance.transform.Find("SafeArea/SubPanelArea");
+        //private static void AssignMenu(GameObject submenuPanelInstance)
+        //{
+        //    Transform header = submenuPanelInstance.transform.Find("SafeArea/HeaderContainer/Header (JUICED)");
+        //    Transform subPanelArea = submenuPanelInstance.transform.Find("SafeArea/SubPanelArea");
 
-            GameObject headerInstance = UnityEngine.Object.Instantiate(headerButton, header);
-            GameObject panelInstance = UnityEngine.Object.Instantiate(panel, subPanelArea);
+        //    GameObject headerInstance = UnityEngine.Object.Instantiate(headerButton, header);
+        //    GameObject panelInstance = UnityEngine.Object.Instantiate(panel, subPanelArea);
 
-            FixBlurShader(panelInstance.transform.Find("Scroll View/BlurPanel").gameObject);
-            panelInstance.SetActive(false);
-            RoR2.UI.HGButton leButton = headerInstance.GetComponent<RoR2.UI.HGButton>();
-            RoR2.UI.HGHeaderNavigationController leHeader = submenuPanelInstance.GetComponent<RoR2.UI.HGHeaderNavigationController>();
-            leButton.onClick.AddListener((delegate ()
-            {
-                leHeader.ChooseHeaderByButton(leButton);
-            }));
+        //    FixBlurShader(panelInstance.transform.Find("Scroll View/BlurPanel").gameObject);
+        //    panelInstance.SetActive(false);
+        //    RoR2.UI.HGButton leButton = headerInstance.GetComponent<RoR2.UI.HGButton>();
+        //    RoR2.UI.HGHeaderNavigationController leHeader = submenuPanelInstance.GetComponent<RoR2.UI.HGHeaderNavigationController>();
+        //    leButton.onClick.AddListener((delegate ()
+        //    {
+        //        leHeader.ChooseHeaderByButton(leButton);
+        //    }));
 
-            if (panelInstance.GetComponent<RoR2.UI.SettingsPanelController>())
-                panelInstance.GetComponent<RoR2.UI.SettingsPanelController>().revertButton = submenuPanelInstance.transform.Find("SafeArea/FooterContainer/FooterPanel, M&KB/RevertAndBack (JUICED)/NakedButton (Revert)").GetComponent<RoR2.UI.HGButton>();
+        //    if (panelInstance.GetComponent<RoR2.UI.SettingsPanelController>())
+        //        panelInstance.GetComponent<RoR2.UI.SettingsPanelController>().revertButton = submenuPanelInstance.transform.Find("SafeArea/FooterContainer/FooterPanel, M&KB/RevertAndBack (JUICED)/NakedButton (Revert)").GetComponent<RoR2.UI.HGButton>();
 
-            if (submenuPanelInstance.GetComponent<RoR2.UI.UILayerKey>())
-            {
-                if (panelInstance.GetComponent<RoR2.UI.HGButtonHistory>())
-                    panelInstance.GetComponent<RoR2.UI.HGButtonHistory>().requiredTopLayer = submenuPanelInstance.GetComponent<RoR2.UI.UILayerKey>();
-                if (panelInstance.GetComponent<RoR2.UI.HGScrollRectHelper>())
-                    panelInstance.GetComponent<RoR2.UI.HGScrollRectHelper>().requiredTopLayer = submenuPanelInstance.GetComponentInChildren<RoR2.UI.UILayerKey>();
-            }
+        //    if (submenuPanelInstance.GetComponent<RoR2.UI.UILayerKey>())
+        //    {
+        //        if (panelInstance.GetComponent<RoR2.UI.HGButtonHistory>())
+        //            panelInstance.GetComponent<RoR2.UI.HGButtonHistory>().requiredTopLayer = submenuPanelInstance.GetComponent<RoR2.UI.UILayerKey>();
+        //        if (panelInstance.GetComponent<RoR2.UI.HGScrollRectHelper>())
+        //            panelInstance.GetComponent<RoR2.UI.HGScrollRectHelper>().requiredTopLayer = submenuPanelInstance.GetComponentInChildren<RoR2.UI.UILayerKey>();
+        //    }
 
-            RoR2.UI.HGHeaderNavigationController hnc = submenuPanelInstance.GetComponent<RoR2.UI.HGHeaderNavigationController>();
-            RoR2.UI.HGHeaderNavigationController.Header sloppytoppy = new RoR2.UI.HGHeaderNavigationController.Header
-            {
-                headerButton = headerInstance.GetComponent<RoR2.UI.HGButton>(),
-                headerName = "Turbo",
-                tmpHeaderText = headerInstance.GetComponentInChildren<TextMeshProUGUI>(),
-                headerRoot = panelInstance
-            };
-            HG.ArrayUtils.ArrayAppend(ref hnc.headers, sloppytoppy);
+        //    RoR2.UI.HGHeaderNavigationController hnc = submenuPanelInstance.GetComponent<RoR2.UI.HGHeaderNavigationController>();
+        //    RoR2.UI.HGHeaderNavigationController.Header sloppytoppy = new RoR2.UI.HGHeaderNavigationController.Header
+        //    {
+        //        headerButton = headerInstance.GetComponent<RoR2.UI.HGButton>(),
+        //        headerName = "Turbo",
+        //        tmpHeaderText = headerInstance.GetComponentInChildren<TextMeshProUGUI>(),
+        //        headerRoot = panelInstance
+        //    };
+        //    HG.ArrayUtils.ArrayAppend(ref hnc.headers, sloppytoppy);
 
-            int lastOption = header.Find("GenericHeaderButton (Graphics)").GetSiblingIndex() + 1;
-            headerInstance.transform.SetSiblingIndex(lastOption);
-            header.Find("GenericGlyph (Right)").SetSiblingIndex(headerInstance.transform.GetSiblingIndex() + 1);
-        }
+        //    int lastOption = header.Find("GenericHeaderButton (Graphics)").GetSiblingIndex() + 1;
+        //    headerInstance.transform.SetSiblingIndex(lastOption);
+        //    header.Find("GenericGlyph (Right)").SetSiblingIndex(headerInstance.transform.GetSiblingIndex() + 1);
+        //}
 
         private static void FixBlurShader(GameObject panel)
         {
@@ -201,22 +201,22 @@ namespace TurboEdition.Misc
                 AssignHUDElement(camera.hud, objectToInstantiate, parent);
         }
 
-        private static IEnumerator AddSettingsMenuCoroutine(RoR2.UI.MainMenu.SubmenuMainMenuScreen trolled) //Me getting trolled by one single line of code
-        {
-            yield return new WaitForEndOfFrame();
-            if (trolled == null)
-                TELog.LogW("Something went wrong when getting SubmenuMainMenuScreen");
-            else
-                AssignMenu(trolled.submenuPanelInstance);
-        }
+        //private static IEnumerator AddSettingsMenuCoroutine(RoR2.UI.MainMenu.SubmenuMainMenuScreen trolled) //Me getting trolled by one single line of code
+        //{
+        //    yield return new WaitForEndOfFrame();
+        //    if (trolled == null)
+        //        TELog.LogW("Something went wrong when getting SubmenuMainMenuScreen");
+        //    else
+        //        AssignMenu(trolled.submenuPanelInstance);
+        //}
 
-        private static IEnumerator AddSettingsMenuCoroutine(RoR2.UI.PauseScreenController trolled) //Me getting trolled by one single line of code
-        {
-            yield return new WaitForEndOfFrame();
-            if (trolled == null)
-                TELog.LogW("Something went wrong when getting PauseScreenController");
-            else
-                AssignMenu(trolled.submenuObject);
-        }
+        //private static IEnumerator AddSettingsMenuCoroutine(RoR2.UI.PauseScreenController trolled) //Me getting trolled by one single line of code
+        //{
+        //    yield return new WaitForEndOfFrame();
+        //    if (trolled == null)
+        //        TELog.LogW("Something went wrong when getting PauseScreenController");
+        //    else
+        //        AssignMenu(trolled.submenuObject);
+        //}
     }
 }
