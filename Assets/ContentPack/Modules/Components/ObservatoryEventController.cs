492b4231cb95d30bdc643651db673ea8e29029ea
Anreol <12176932+Anreol@users.noreply.github.com>
2023-11-06 19:59:25 +0100

    This works with the latest update
    But is built before the latest update, haha!

--- Assets/ContentPack/Modules/Components/ObservatoryEventController.cs
+++ Assets/ContentPack/Modules/Components/ObservatoryEventController.cs
@@ -1,16 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace TurboEdition.Components
 {
     internal class ObservatoryEventController
     {
-        public void test()
+        public GameObject rainParticleGameObject;
+        public GameObject thunderMeshGameObject;
+
+        public string wwiseThunderPlayEvent;
+
+        private uint wwiseThunderID;
+
+        public void OnEnable()
         {
-            //AkSoundEngine
+            //For Thunder, play the sound event.
+           // wwiseThunderID = AkSoundEngine.PostEvent(wwiseThunderPlayEvent, )
+        }
+        public void Update()
+        {
+            //Check if the RCTP value has been changed in WWise
+
+            //Do things if the RCTP value has changed
+            RainStart();
+        }
+
+        public void OnDisable()
+        {
+            if (wwiseThunderID != 0)
+            {
+                AkSoundEngine.StopPlayingID(wwiseThunderID);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void RainStart()
+        {
+
+        }
+
+        /// <summary>
+        /// Makes the <see cref="thunderMeshGameObject"/> appear and disappear. Meant to be called as a callback from a Wwise event.
+        /// </summary>
+        public void ThunderFlash()
+        {
+            //Get the mesh and change the alpha of the material rapidly
         }
     }
 }
