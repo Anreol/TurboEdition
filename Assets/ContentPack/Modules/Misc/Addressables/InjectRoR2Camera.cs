a2018fc4f16589acf8f92685d9fc432c901bc4dd
Anreol <12176932+Anreol@users.noreply.github.com>
2023-11-15 00:48:06 +0100

    so much patience

--- Assets/ContentPack/Modules/Misc/Addressables/InjectRoR2Camera.cs
+++ Assets/ContentPack/Modules/Misc/Addressables/InjectRoR2Camera.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 namespace TurboEdition.Components
@@ -6,9 +7,37 @@ namespace TurboEdition.Components
     [ExecuteAlways]
     public class InjectRoR2Camera : MonoBehaviour
     {
-        private void Awake()
+#if UNITY_EDITOR
+
+        private void OnEnable()
         {
+            CleanUpChildren();
+
             var cam = Instantiate(Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Core/Main Camera.prefab").WaitForCompletion(), gameObject.transform);
+            cam.gameObject.transform.localPosition = Vector3.zero;
+            cam.gameObject.transform.localRotation = Quaternion.identity;
         }
+
+        private void OnDisable()
+        {
+            CleanUpChildren();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void CleanUpChildren()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (Application.isPlaying)
+                    Destroy(transform.GetChild(i).gameObject);
+                else
+                {
+                    //This is to be used in the editor only!
+                    DestroyImmediate(transform.GetChild(i).gameObject);
+                }
+            }
+        }
+
+#endif
     }
 }
